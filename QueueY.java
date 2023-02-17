e003104905302c24dd70417c5862b32575c922c3
mdrafin008 <mdrafin008@gmail.com>
2023-02-17 08:12:32 +0600

    sinfgular queue

---
+++ QueueY.java
@@ -0,0 +1,75 @@
+public class QueueY{
+   static class Queue{
+    static int arr[];
+     static int size;
+     static int rear = -1;
+    Queue(int size){
+        arr = new int[size];
+          this.size = size;
+    }
+public static boolean isEmpty(){
+     return rear == -1;
+}
+
+   //equeue
+   public static void add (int data){
+    if(rear==size-1){
+        System.out.println("full queue");
+        return;
+
+    }
+
+    rear++;
+    arr[rear]=data;
+
+   }
+
+   //dequeue
+   public static int remove(){
+       if(isEmpty()){
+        System.out.println("Empty queue");
+        return -1;
+
+    }
+ 
+      int front = arr[0];
+      for(int i = 0; i<rear;i++){
+        arr[i]=arr[i+1];
+
+      }
+        rear--;
+        return front;
+
+   }
+
+   //peek
+   public static int peek(){
+    if(isEmpty()){
+        System.out.println("Empty queue");
+        return -1;
+
+    }
+ 
+      
+     return arr[0];
+
+      
+       
+       
+   }
+
+   } 
+    public static void main (String args[]){
+          Queue q = new Queue(5);
+          q.add(1);
+          q.add(2);
+          q.add(3);
+         
+
+          while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+          }
+
+    }
+}
